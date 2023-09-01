a17c1887af881431c4adc7c7fd2095e94468d627
Ernesto Gámez <e-gamez@hotmail.es>
2023-09-01 17:15:22 +0200

    initial commi

---
+++ server/index.js
@@ -0,0 +1,12 @@
+// Configuración de un servidor de la librería express (instalada)
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const staticFilesPath = path.resolve(__dirname, './public');
+app.use('/', express.static(staticFilesPath));
+
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+  console.log(`App running on http://localhost:${PORT}`);
+});
